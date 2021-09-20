f4dd2ae869485013ba38ec9df46efd40cc06fe33
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-21 01:56:55 +0200

    Update 07_Polymorphism.py

--- chap 01/07_Polymorphism.py
+++ chap 01/07_Polymorphism.py
@@ -1,4 +1,5 @@
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Dynamic Polymorphism : Uses the same inter for methods on different types of objects
-#
+# The other common form of polymorphism is called static or compile-time polymorphism.
+# Overloading allows you to implement multiple methods within a class that have the same name, but a different set of input parameters.
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

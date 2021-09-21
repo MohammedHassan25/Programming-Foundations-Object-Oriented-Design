c5d75be5cad5b21b07ee518774c3f122e40c7a04
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-21 08:02:51 +0200

    chap 02 : Requierments
    chap 02 : Requierments

---
+++ chap 02/01_Defining requirements.py
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# The first step to any design process is to gather your requirements
+# What we're doing here, defining requirements, has nothing to do with object orientation
+# On the first pass, only focus on capturing the absolute minimum set of requirements.
+# Not the things that are optional, or nice to have. Just the bare necessities, your minimum viable product
+# ---------------------------------------------------------------------------------------------------------------------
---
+++ chap 02/02_FURPS+ requirements.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+# One commonly used model for classifying software quality attributes is the acronym FURPS (functionality, usability, reliability, performance, supportability)
+# functionality : It is The core of what the customer wants (Capability, Reusability, Security)
+# Usability affects the person who will be using the program (Human Factors, Aesthetics, Consistency, Documentation)
+# Reliability : (Availability, Failure Rate and Duration, Predictability)
+# Performance : (Speed, Efficiency, Resource Consumption, Scalability)
+# Supportability : (Testablity, Extensibilitiy, Serviceability, Configurability)
+# FURPS+ :(Desgin, Implementation, Interface, Physical)
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------

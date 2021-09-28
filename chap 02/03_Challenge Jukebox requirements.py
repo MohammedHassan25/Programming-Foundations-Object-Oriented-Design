72ebfa20361a4b3c507e961ff7c2ac2b228cd59f
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-28 06:12:55 +0200

    chap 02 : Requierments
    chap 02 : Requierments

---
+++ chap 02/03_Challenge Jukebox requirements.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------------------------------
+# Functional requirements for Jukebox
+# The application must :-
+# 1- Don't let just one person pick all the music
+# 2- let the user select an album from the available music library, and then choose individual songs from that album
+# 3- contain a music player
+# --------------------------------------------------------------------------------------------------------------------
+# Non-functional requirements for Jukebox
+# The application should :-
+# 1- be free to use.
+# 2- be A vailable 24/7
+# 3- be comfortable on the eyes
+# --------------------------------------------------------------------------------------------------------------------
